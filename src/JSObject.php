e4991e1533b42c51d85c863a5ff5debac9bf0a0b
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-05 15:11:57 -0400

    Added undefined and JSObject class

---
+++ src/JSObject.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class JSObject implements Stringable {
+  // TODO: Implement object methods
+  function __toString() {
+    // TODO: Implement toString()
+    return '';
+  }
+}
---
+++ src/undefined.php
@@ -0,0 +1,3 @@
+<?php
+
+const undefined = null;
