62a9e26aa198b8cae63136c1482970d65e5f3667
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-27 18:36:24 -0400

    silent Symbol phpstan errors

--- src/Globals/Symbol.php
+++ src/Globals/Symbol.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 /**
  * Returns a new unique Symbol value.
  * @param string|int|float|null $description
+ * @return mixed TODO: Specify return type
  */
 function Symbol($description = null) {
+  return $description;
 }
